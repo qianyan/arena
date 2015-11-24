43b1bbafa43cf417d1c96dc0798b2cf745a10cc8
wjchen <wjchen@thoughtworks.com>
2015-11-24 18:42:46 +0800

    add player object

---
+++ src/main/java/oo/Player.java
@@ -0,0 +1,33 @@
+package oo;
+
+public class Player {
+    private String name;
+    private int blood;
+    private int damage;
+
+    public Player(String name, int blood, int damage) {
+        this.name = name;
+        this.blood = blood;
+        this.damage = damage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBlood() {
+        return blood;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void attack(Player otherPlayer) {
+        otherPlayer.blood -= damage;
+    }
+
+    public boolean isAlive() {
+        return blood > 0;
+    }
+}
---
+++ src/test/java/oo/PlayerTest.java
@@ -0,0 +1,34 @@
+package oo;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+    @Test
+    public void shouldCreatePlayerSuccessfully() {
+        Player player = new Player("王二麻子", 100, 10);
+
+        assertThat(player.getName(), is("王二麻子"));
+        assertThat(player.getBlood(), is(100));
+        assertThat(player.getDamage(), is(10));
+    }
+
+    @Test
+    public void shouldCanAttackOtherPlayer() {
+        Player attacker = new Player("张三", 100, 10);
+        Player victim = new Player("李四", 80, 20);
+
+        attacker.attack(victim);
+
+        assertThat(victim.getBlood(), is(80 - attacker.getDamage()));
+    }
+
+    @Test
+    public void shouldDeathWhenBloodLessThan0() {
+        Player die = new Player("王二麻子", -1, 0);
+
+        assertFalse(die.isAlive());
+    }
+}
